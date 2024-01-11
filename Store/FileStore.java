fc8d129111219607a68629b1116b9d10ccaa2a52
Niyaz Safiullin <niyaz_safiullin@epam.com>
2024-01-11 20:04:29 +0100

    Add input/output from console

--- Store/FileStore.java
+++ Store/FileStore.java
@@ -1,19 +1,46 @@
 package Store;
 
+import java.util.Scanner;
+
 public class FileStore implements IStore {
 
     @Override
     public void save() {
+        Scanner getInput = new Scanner(System.in);
+
+        System.out.println("Enter something:");
+        String userInput = getInput.nextLine();
+
+        System.out.println(userInput);
+        getInput.close();
+
         System.out.println("Saved");
     }
 
     @Override
     public void get() {
+
+        Scanner getInput = new Scanner(System.in);
+
+        System.out.println("Enter something:");
+        String userInput = getInput.nextLine();
+
+        System.out.println(userInput);
+        getInput.close();
+
         System.out.println("Got");
     }
 
     @Override
     public void delete() {
+        Scanner getInput = new Scanner(System.in);
+
+        System.out.println("Enter something:");
+        String userInput = getInput.nextLine();
+
+        System.out.println(userInput);
+        getInput.close();
+
         System.out.println("Deleted");
     }
 }
